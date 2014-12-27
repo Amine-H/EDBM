1689cc053bf59c2bebf573a7eea9be8e42fa369e
Amine Hakkou <amine.hakkou.pro@gmail.com>
2014-12-27 17:40:44 +0000

    corrected some merge stuff,ali watch language this is public!

--- inc/home.php
+++ inc/home.php
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-<?php if(!isset($isIndex)){die('');} ?>
+<?php if(!isset($isIndex)){die('');}?>
 hello,welcome to Easy Database Manager!
-=======
-<?php if(!isset($isIndex)){die('error has been made');}
+<?php
 require_once("/inc/tableMenu.php");
 ?>
->>>>>>> 874540eac1cc6da99d0e046120d3bf7d3499cbe8
--- inc/tableMenu.php
+++ inc/tableMenu.php
@@ -1,6 +1,6 @@
 <div class="container" >
 	<div class="row">
-	  <div class="col-md-12 text-center">Esay DAtabase Managr Bitch </div>
+	  <div class="col-md-12 text-center">Esay DAtabase Managr </div>
 	</div>
 	<div class="row">
 	  <div class="col-md-4">.col-xs-6 .col-md-4</div>
