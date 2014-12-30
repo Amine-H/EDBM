494f78c1f0c0c6aa7e091ae0b00866e998f8e30b
Amine Hakkou <amine.hakkou.pro@gmail.com>
2014-12-30 00:14:58 +0000

    most of the work i done now was on procedure.php mostly javascript and jquery stuff that allows the user to add of remove params and i can say ,i finally fixed procedure table css bug! horray

--- inc/procedure.php
+++ inc/procedure.php
@@ -8,11 +8,36 @@ require_once('/inc/procedure.class.php');
 	params_count=0;
 	function addParam()
 	{
-		var params_table = $('#params_table');
+		var param='<tr class="param" id="param_'+params_count+'">'+
+	 			'<td>'+
+					'<select name="param_dir['+params_count+']" id="param_dir_'+params_count+'">'+
+	                    '<option>IN</option>'+
+	                    '<option>OUT</option>'+
+	                    '<option>INOUT</option>'+
+                	'</select>'+
+	 			'</td>'+
+	 			'<td><input name="param_name['+params_count+']" id="param_name_'+params_count+'" type="text"></td>'+
+	 			'<td>'+
+					'<select name="param_type['+params_count+']">'+
+						'<option>INT</option>'+
+						'<option>VARCHAR</option>'+
+						'<option>DATE</option>'+
+					'</select>'+
+	 			'</td>'+
+	 			'<td><input name="param_length['+params_count+']" id="param_length_'+params_count+'" type="text"></td>'+
+	 			'<td><button type="button" class="btn btn-danger btn-lg" onclick="javascript:removeParam('+params_count+');"><span class="glyphicon glyphicon-remove"></span></button></td>'+
+	 		'</tr>';
+	 	$('#button').before(param);
+	 	$('#param_'+params_count).hide().fadeIn('normal');
+	 	params_count++;
 	}
 	function removeParam(input)
 	{
-
+		$('#param_'+input).fadeOut('normal');
+		setTimeout(function()
+		{
+			$('#param_'+input).remove();
+		},1000);
 	}
 </script>
 <form class="form-horizontal" action="/_procedure" method="POST">
@@ -31,7 +56,7 @@ require_once('/inc/procedure.class.php');
 <div class="control-group">
 	<label class="control-label">Paramètres</label>
 	<div class="controls">
-	 	<table class="table table-striped table-nonfluid" style="display:relative;" id="params_table">
+	 	<table class="table table-striped table-nonfluid" id="params_table">
 	 		<tr>
 	 			<th>Direction</th>
 	 			<th>Nom</th>
@@ -39,28 +64,7 @@ require_once('/inc/procedure.class.php');
 	 			<th>Taille/Valeurs</th>
 	 			<th></th>
 	 		</tr>
-	 		<tr class="param">
-	 			<td>
-					<select name="param_dir[0]" id="param_dir_0">
-	                    <option>IN</option>
-	                    <option>OUT</option>
-	                    <option>INOUT</option>
-                	</select>
-	 			</td>
-	 			<td><input name="param_name[0]" id="param_name_0" type="text" value=""></td>
-	 			<td>
-					<select name="param_type[0]">
-						<option>INT</option>
-						<option>VARCHAR</option>
-						<option>DATE</option>
-					</select>
-	 			</td>
-	 			<td><input name="param_length[0]" id="param_length_0" type="text" value=""></td>
-	 			<td><button type="button" class="btn btn-default btn-lg">
-					<span class="glyphicon glyphicon-remove"></span></button>
-				</td>
-	 		</tr>
-	 		<tr>
+	 		<tr id="button">
 	 			<td colspan="5"><button type="button" class="btn btn-default" onclick="javascript:addParam();">Ajouter un paramètre</button></td>
 	 		</tr>
 	 	</table>
@@ -69,12 +73,12 @@ require_once('/inc/procedure.class.php');
 <div class="control-group">
 	<label class="control-label">Code</label>
 	<div class="controls">
-	 	<textarea class="form-control" rows="5"name="code" id="code"></textarea>
+	 	<textarea class="form-control" rows="5" name="code" id="code"></textarea>
 	</div>
 </div>
 <div class="control-group">
 	<div class="control">
-		<button type="button" class="btn btn-primary">Ajouter</button>
+		<button type="submit" class="btn btn-primary">Ajouter</button>
 	</div>
 </div>
 </fieldset>
@@ -89,12 +93,13 @@ if(isset($params[0]))
 	{//we got the data we should use javascript to fill the table with it
 		?>
 		<script type="text/javascript">
-		var procedure = <?php echo json_encode($procedure); ?>;
-		$('#name').val(procedure.name);
-		$('#param_dir_0').val(procedure.params[0]);
-		$('#param_name_0').val(procedure.params[1]);
-		$('#param_length_0').val(procedure.params[2]);
-		$('#code').val(procedure.code);
+			var procedure = <?php echo json_encode($procedure); ?>;
+			$('#name').val(procedure.name);
+			addParam();
+			$('#param_dir_0').val(procedure.params[0]);
+			$('#param_name_0').val(procedure.params[1]);
+			$('#param_length_0').val(procedure.params[2]);
+			$('#code').val(procedure.code);
 		</script>
 		<?php
 	}
