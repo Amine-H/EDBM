48078cb0ce48cba7fec9e4a1d699a7902ba5a338
ALI ELOUAI <happy-ali@live.fr>
2015-01-13 03:39:30 +0000

    working on table

--- inc/tableAdd.php
+++ inc/tableAdd.php
@@ -16,12 +16,12 @@ require_once('/inc/table.class.php');
 		<?php
 		$m = 2;//compt des nbr de arry
 		for($i=0;$i<$m;$i++){?>
-		<tr >
+			<tr >
 			<th>
-				<input type="text" class="form-control  input-lg" id="field" name="field">
+				<input type="text" class="form-control  input-lg" id="field<?php echo $i?>" name="field<?php echo $i?>">
 			</th>
 			<th>
-				<select name="type" id="type" class="form-control">
+				<select name="type<?php echo $i?>" id="type<?php echo $i?>" class="form-control">
 				  <option value="int" selected>Int</option>
 				  <option value="varchr">Varchar</option>
 				  <option value="text" >Text</option>
@@ -30,16 +30,16 @@ require_once('/inc/table.class.php');
 				</select>
 			</th>
 			<th >
-				<input type="number" class="form-control" id="taille" name="taille"  min="1" max="2000">
+				<input type="number" class="form-control" id="taille<?php echo $i?>" name="taille<?php echo $i?>"  min="1" max="2000">
 			</th>
 			<th >
-				<input type="checkbox" class="form-control" id="null" name="null" >
+				<input type="checkbox" class="form-control" id="null<?php echo $i?>" name="null<?php echo $i?>" >
 			</th>
 			<th >
-				<input type="text" class="form-control" id="default" name="default" >
+				<input type="text" class="form-control" id="default<?php echo $i?>" name="default<?php echo $i?>" >
 			</th>
 			<th>
-				<select name="type" id="type" class="form-control">
+				<select name="type<?php echo $i?>" id="type<?php echo $i?>" class="form-control">
 				  <option value="none" selected>---</option>
 				  <option value="primary key">PRIMARY KEY</option>
 				  <option value="index">INDEX</option>
@@ -47,7 +47,7 @@ require_once('/inc/table.class.php');
 				</select>
 			</th>
 			<th >
-				<input type="checkbox" class="form-control" id="auto_increment" name="auto_increment" >
+				<input type="checkbox<?php echo $i?>" class="form-control" id="auto_increment" name="auto_increment" >
 			</th>
 			
 
